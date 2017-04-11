d447e3326c6e409c6198d753fdc347a2aa7f36f3
Megan Kaye <megan.kaye38@gmail.com>
2017-04-11 16:20:43 -0400

    added xcode proj

---
+++ TriviaApp/TriviaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Megan Kaye on 4/11/17.
+//  Copyright © 2017 Winsor Coding Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
